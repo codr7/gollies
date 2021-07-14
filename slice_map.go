8839b75b8b2c91eb030841382bb0aea3a9be1496
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-07-14 03:17:57 +0200

    Renames

--- slice_map.go
+++ slice_map.go
@@ -5,8 +5,8 @@ type SliceMapItem = struct {
 }
 
 type SliceMap struct {
-	Compare Compare
-	Items []SliceMapItem
+	compare Compare
+	items []SliceMapItem
 }
 
 func NewSliceMap(cmp Compare) *SliceMap {
@@ -14,7 +14,7 @@ func NewSliceMap(cmp Compare) *SliceMap {
 }
 
 func (m *SliceMap) Init(cmp Compare) *SliceMap {
-	m.Compare = cmp
+	m.compare = cmp
 	return m
 }
 
@@ -23,9 +23,9 @@ func (m *SliceMap) Index(key interface{}) (int, interface{}) {
 
 	for min < max {
 		i := (min+max)/2
-		it := m.Items[i]
+		it := m.items[i]
 		
-		switch m.Compare(key, it.Key) {
+		switch m.compare(key, it.Key) {
 		case Lt:
 			max = i
 		case Eq:
@@ -50,9 +50,9 @@ func (m *SliceMap) Add(key interface{}, val interface{}) interface{} {
 		return found
 	}
 
-	m.Items = append(m.Items, SliceMapItem{})
-	copy(m.Items[i+1:], m.Items[i:])
-	m.Items[i] = SliceMapItem{key, val}
+	m.items = append(m.items, SliceMapItem{})
+	copy(m.items[i+1:], m.items[i:])
+	m.items[i] = SliceMapItem{key, val}
 	return nil
 }
 
@@ -60,12 +60,12 @@ func (m *SliceMap) Remove(key interface{}) interface{} {
 	i, found := m.Index(key)
 
 	if found != nil {
-		m.Items = m.Items[:i+copy(m.Items[i:], m.Items[i+1:])]
+		m.items = m.items[:i+copy(m.items[i:], m.items[i+1:])]
 	}
 
 	return found
 }
 
 func (m SliceMap) Len() int {
-	return len(m.Items)
+	return len(m.items)
 }
